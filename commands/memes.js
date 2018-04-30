8230d9ed52bebf23301aa52ea23903a172c0412a
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-01 00:23:13 +0300

    Create memes.js

---
+++ commands/memes.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const memesDir = "./memes";
+
+var length = 0;
+
+fs.readdir(memesDir, (err, files) => {
+	length = files.length;
+});
+
+module.exports.run = async (bot, message, args) => {
+	 let channel = message.channel;
+
+	 var r = Math.floor(Math.random() * length);
+   
+   channel.send("", { files: [`./memes/${r}.jpg`]});
+} 
+
+module.exports.help = {
+    name: "memes"
+}
