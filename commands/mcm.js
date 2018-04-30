9822238eb459efb27c240ebd61096eb4c1443e7d
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-04-30 15:19:43 -0700

    Update mcm.js

--- commands/mcm.js
+++ commands/mcm.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  let bicon = bot.user.displayavatarURL;
   let mcmembed = new Discord.RichEmbed()
   .setDescription("MCM Link")
+  .setThumbnail(bicon)
   .setColor("#e44444")
   .addField("LINK >", "https://www.mc-market.org/members/97550/", true);
 
