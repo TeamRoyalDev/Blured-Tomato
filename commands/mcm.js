9fa576137f6009802c18cc826aaf10cb71a819f8
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-01 00:54:42 +0300

    Update mcm.js

--- commands/mcm.js
+++ commands/mcm.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
-    let user = message.author;
-    
-    user.send(`💻 Setups: https://www.mc-market.org/threads/332335/ \nPortfolio: \n1- https://discord.gg/sSbXq6y \n2- https://discord.gg/c3jvXwu \n3- https://discord.gg/NxRCTm9 \n4- https://discord.gg/S9ZzYCg  \n5- https://discord.gg/BqsBssc`);
+module.exports.run = async (bot, message, args) => { 
+    message.author.send(`💻 Setups: https://www.mc-market.org/threads/332335/ \nPortfolio: \n1- https://discord.gg/sSbXq6y \n2- https://discord.gg/c3jvXwu \n3- https://discord.gg/NxRCTm9 \n4- https://discord.gg/S9ZzYCg  \n5- https://discord.gg/BqsBssc`);
 }
 
 module.exports.help = {
