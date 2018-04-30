376e6a6357438603271da5e7ec4332624108dd3f
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-04-30 15:11:54 -0700

    Update mcm.js

--- commands/mcm.js
+++ commands/mcm.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let mcmembed = new Discord.RichEmbed()
   .setDescription("MCM Link")
-  .setColor("#930000")
+  .setColor("#e44444")
   .addField("LINK >", "https://www.mc-market.org/members/97550/", true);
 
   return message.channel.send(mcmembed);
