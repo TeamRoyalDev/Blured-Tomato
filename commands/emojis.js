2b650dc5c8733933d59816dcf64a81b751048de4
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-01 00:23:33 +0300

    Create emojis.js

---
+++ commands/emojis.js
@@ -0,0 +1,16 @@
+module.exports.run = async (bot, message, args) => {
+    let channel = message.channel;
+    let server = message.guild;
+    let user = message.member;
+    
+    if(user.hasPermission("MANAGE_GUILD")) {
+    	if(args[0] && args[1]) {
+      	guild.createEmoji(args[0], args[1]);
+        channel.send(`An emoji has been created by ${user.username} with the name: ${args[1]}.`);
+      } else channel.send(`Insufficient arguments.\n\nUsage: b!emojis <url> <name>`);
+    } else channel.send("Insufficient permissions.");
+}
+
+module.exports.help = {
+    name: "emojis"
+}
