2306f9db06441e5033c1c86338dbd3c2b2126441
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-04-30 14:51:27 -0700

    Update mcm.js

--- commands/mcm.js
+++ commands/mcm.js
@@ -3,9 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let user = message.author;
     
-    user.send("🔗 MCM: https://www.mc-market.org/members/97550/");
+    user.send(`🔗 MCM: https://www.mc-market.org/members/97550/`);
 }
 
 module.exports.help = {
-    name: "mcm"
+    name: "setups"
 }
