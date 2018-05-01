3ac789e7369a01fed9a5ad967fe6b689cdadc90e
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-01 13:30:54 -0700

    Update setups.js

--- commands/setups.js
+++ commands/setups.js
@@ -3,7 +3,19 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let user = message.author;
     
-    user.send(`💻 Setups: https://www.mc-market.org/threads/332335/ \nPortfolio: \n1- https://discord.gg/sSbXq6y \n2- https://discord.gg/c3jvXwu \n3- https://discord.gg/NxRCTm9 \n4- https://discord.gg/S9ZzYCg  \n5- https://discord.gg/BqsBssc`);
+   let setupEmbed = new Discord.RichEmbed()
+  .setAuthor(":computer: Setups")
+  .setColor("#800080")
+  .addField("**1** _-_ https://discord.gg/sSbXq6y")
+  .addField("**2** _-_ https://discord.gg/c3jvXwu")
+  .addField("**3** _-_ https://discord.gg/NxRCTm9")
+  .addField("**4** _-_ https://discord.gg/S9ZzYCg")
+  .addField("**5** _-_ https://discord.gg/BqsBssc")
+   
+  let user = message.author;
+
+  user.send(setupEmbed);
+    
 }
 
 module.exports.help = {
