f97a547dce8cc42afcede5ae4db6cc9e7af57c52
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-01 13:55:25 -0700

    Update setups.js

--- commands/setups.js
+++ commands/setups.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   .setAuthor("Blured Setups")
   .setColor("#800080")
   .setDescription("**1** _-_ https://discord.gg/sSbXq6y\n**2** _-_ https://discord.gg/c3jvXwu\n**3** _-_ https://discord.gg/NxRCTm9\n**4** _-_ https://discord.gg/S9ZzYCg\n**5** _-_ https://discord.gg/BqsBssc")
-  .setFooter("Setups from Blured, ©Team Royal", "https://i.imgur.com/79sCH4c.png");
+  .setFooter("`©`Setups from Blured", "https://i.imgur.com/79sCH4c.png");
    
   let user = message.author;
 
