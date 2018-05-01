48bb7d49c45a48423edf4d0fd02df1bd47e4bbc5
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-01 13:56:10 -0700

    Update setups.js

--- commands/setups.js
+++ commands/setups.js
@@ -2,11 +2,11 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     
-   let setupEmbed = new Discord.RichEmbed()©
+   let setupEmbed = new Discord.RichEmbed()
   .setAuthor("Blured Setups")
   .setColor("#800080")
   .setDescription("**1** _-_ https://discord.gg/sSbXq6y\n**2** _-_ https://discord.gg/c3jvXwu\n**3** _-_ https://discord.gg/NxRCTm9\n**4** _-_ https://discord.gg/S9ZzYCg\n**5** _-_ https://discord.gg/BqsBssc")
-  .setFooter("`©`Setups from Blured", "https://i.imgur.com/79sCH4c.png");
+  .setFooter("©Tomato Blured", "https://i.imgur.com/79sCH4c.png");
    
   let user = message.author;
 
