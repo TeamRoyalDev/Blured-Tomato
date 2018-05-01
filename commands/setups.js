6c87ad9e56e58db791916d5576d5e652c5fe6b12
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-01 13:39:13 -0700

    Update setups.js

--- commands/setups.js
+++ commands/setups.js
@@ -5,11 +5,11 @@ module.exports.run = async (bot, message, args) => {
    let setupEmbed = new Discord.RichEmbed()
   .setAuthor(":computer: Setups")
   .setColor("#800080")
-  .addField("**1** _-_ https://discord.gg/sSbXq6y")
-  .addField("**2** _-_ https://discord.gg/c3jvXwu")
-  .addField("**3** _-_ https://discord.gg/NxRCTm9")
-  .addField("**4** _-_ https://discord.gg/S9ZzYCg")
-  .addField("**5** _-_ https://discord.gg/BqsBssc");
+  .setDescription("**1** _-_ https://discord.gg/sSbXq6y")
+  .setDescription("**2** _-_ https://discord.gg/c3jvXwu")
+  .setDescription("**3** _-_ https://discord.gg/NxRCTm9")
+  .setDescription("**4** _-_ https://discord.gg/S9ZzYCg")
+  .setDescription("**5** _-_ https://discord.gg/BqsBssc");
    
   let user = message.author;
 
