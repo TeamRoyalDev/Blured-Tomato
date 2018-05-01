f74ae6d5d8c2ef2a7945cf0d0fa1a938f7123939
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-01 14:29:57 -0700

    Update memes.js

--- commands/memes.js
+++ commands/memes.js
@@ -13,7 +13,7 @@ let channel = message.channel;
 var r = Math.floor(Math.random() * length);
 let embedMsg = new Discord.RichEmbed()
 .setDescription("test1")
-.setImage(`./${r}.jpg`)
+.setImage(`./images/${r}.jpg`)
 
 channel.send(embedMsg);
 
