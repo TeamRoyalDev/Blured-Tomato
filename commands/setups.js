751e516c4e230a9bb005425f779b951be27d0cee
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-01 13:43:17 -0700

    Update setups.js

--- commands/setups.js
+++ commands/setups.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     
    let setupEmbed = new Discord.RichEmbed()
-  .setAuthor(":computer: Setups")
+  .setAuthor("Blured Setups")
   .setColor("#800080")
   .setDescription("**1** _-_ https://discord.gg/sSbXq6y\n**2** _-_ https://discord.gg/c3jvXwu\n**3** _-_ https://discord.gg/NxRCTm9\n**4** _-_ https://discord.gg/S9ZzYCg\n**5** _-_ https://discord.gg/BqsBssc");
    
