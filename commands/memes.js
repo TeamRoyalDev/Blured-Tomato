f9eda7f1380dbee7ae03bea9d38b190373f0b3f6
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-02 14:45:04 -0700

    Update memes.js

--- commands/memes.js
+++ commands/memes.js
@@ -7,12 +7,12 @@ var length = 0;
 fs.readdir(memesDir, (err, files) => {
 	length = files.length;
 });
-
-let pic = codes[r];	
+	
 var codes = ["EDsZott", "1r9Sre9", "Fm5dyoq", "yp6U7e0", "S3sEFt8","oWQgGcJ","EuzNodX","xF9gfTQ","cJC8le4","NhZVYji", "uXRjU2D", "2bK3yQ6", "TCg5ado"];
 
 module.exports.run = async (bot, message, args) => {
 var r = Math.floor(Math.random() * (codes.length));	
+let pic = codes[r];	
 let channel = message.channel;
 let embedMsg = new Discord.RichEmbed()
 .setAuthor("test1")
