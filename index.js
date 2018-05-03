d9759daaf438baaf68400cb3b3ee3b5d09718ac4
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-03 09:26:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
+let cooldown = new Set();
+let cdseconds = 5;
 
 bot.commands = new Discord.Collection();
 
@@ -51,7 +53,37 @@ if(cmd === mcmCmd | cmd === setupsCmd | cmd === memesCmd | cmd === emojisCmd | c
   	let cmdFile = bot.commands.get(cmd.slice(prefix.length));
         cmdFile.run(bot, message, args);
   }
+var codes = ["EDsZott", "1r9Sre9", "Fm5dyoq", "yp6U7e0", "S3sEFt8","oWQgGcJ","EuzNodX","xF9gfTQ","cJC8le4","NhZVYji", "uXRjU2D", "2bK3yQ6",
+         "9HojAvB", "zvTIc8v", "1ovSWO6", "YKMTp7c", "QVUfgYN", "OZtermd", "qOGNqwD", "uLuQYQH", "MnCLOz6", "gXPztQr", "lgkEHWg"];
+var memes = ["Sorry Tilted gotta choose Dusty", "Season 4 Coming Clutch.", "Me: _finally eliminated my opponent with 13 health remaining_\nJohn Wick:", "Thats why i have alot of shoes in my locker.", "Back in the old days.", 
+"Epicgames staff is so nice.", "Being an idiot is now a talent", "That's why I don't have a girlfriend", "Got 999 Wood Anybody need a house ?", "Oh boy!", "Rip those boobs", "That's my luck.", "When you think its gonne be an easy kill.","When you run to the bathroom between games and hear the battle bus horn go off"
+        , "Remember the girl from lazy town? This is her now, feel old yet?", "Gods plan", "When you peek your head out of a base and get sniped"
+        , "Vaulted", "Tag someone who would unlock this Snapchat trophy :joy:", "These are the facts", "Fax", "When your homie dies in duos.", "When there are 5 peoples left in the circle"];
 
+if (cmd.startsWith("b!")) {
+if(cooldown.has(message.author.id)){
+    message.delete();
+    message.reply("Chill you are sending too many messages.")
+}
+if(!message.member.hasPermission("ADMINSTRATOR")){
+    cooldown.add(message.author.id);
+}    
+var r = Math.floor(Math.random() * (codes.length));    
+let pic = codes[r];    
+let meme = memes[r];
+let channel = message.channel;
+let embedMsg = new Discord.RichEmbed()
+.setAuthor(`${meme}`, "https://imgur.com/tg2dtMY.png")
+.setColor(0x00AE86)
+.setImage(`https://imgur.com/${pic}.jpg`);
+
+channel.send(embedMsg);
+
+
+setTimeout(() => {
+    cooldown.delete(message.author.id)
+}, cdseconds * 1000)
+}
 });
 
 bot.login(process.env.BOT_TOKEN);
