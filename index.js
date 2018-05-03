c5e44893d005b254cd9d717b72794f9b98a8a849
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-03 13:03:35 -0700

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ fs.readdir("./commands/", (err, files) =>{
 
 bot.on("ready", async () => {
   console.log(`${bot.user.username} has successfully started. Made By Chelsea & Death.`);
-  bot.user.setActivity("Blured | b!Help", {type: "PLAYING"});
+  bot.user.setActivity("Blured | >help", {type: "PLAYING"});
 
 
 });
