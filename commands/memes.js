a94c2603f46fd3a0efa79bc182c99d8df50dbfda
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-03 07:18:30 -0700

    Update memes.js

--- commands/memes.js
+++ commands/memes.js
@@ -10,7 +10,7 @@ fs.readdir(memesDir, (err, files) => {
 	
 var codes = ["EDsZott", "1r9Sre9", "Fm5dyoq", "yp6U7e0", "S3sEFt8","oWQgGcJ","EuzNodX","xF9gfTQ","cJC8le4","NhZVYji", "uXRjU2D", "2bK3yQ6"];
 var memes = ["Sorry Tilted gotta choose Dusty", "Season 4 Coming Clutch.", "Double Pump is back ! Hooray", "Thats why i have alot of shoes in my locker.", "Back in the old days.", 
-"Epicgames staff is so nice.", "Being an idiot is now a talent", "That's why I don't have a girlfriend", "No materials left.", "Oh boy!"];
+"Epicgames staff is so nice.", "Being an idiot is now a talent", "That's why I don't have a girlfriend", "Got 999 Wood Anybody need a house ?", "Oh boy!", "Rip those boobs", "That's my luck."];
 
 module.exports.run = async (bot, message, args) => {
 var r = Math.floor(Math.random() * (codes.length));	
