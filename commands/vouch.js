ff65a7b9ed99fb6364ba768f1d50e8fd36ade582
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-03 13:09:24 -0700

    Create vouch.js

---
+++ commands/vouch.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  let bicon = bot.user.displayavatarURL;
+  let vouchembed = new Discord.RichEmbed()
+  .setAuthor("༺▬▬ ❰ VOUCH ❱ ▬▬༻")
+  .setThumbnail(bicon)
+  .setColor("#e44444")
+  .setDescription("Here is my Profile if u are able to leave a Vouch. Have a good day!");
+  .setDescription("https://www.mc-market.org/members/97550/");
+  .addField("༺▬▬ ❰ VOUCH ❱ ▬▬༻"");
+
+  return message.channel.send(vouchembed);
+}
+
+module.exports.help = {
+  name:"vouch"
+}
