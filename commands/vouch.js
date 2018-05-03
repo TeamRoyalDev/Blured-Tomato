1595cdabbd436faa97cac916d1ef810d779caa3e
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-03 14:42:56 -0700

    Update vouch.js

--- commands/vouch.js
+++ commands/vouch.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   
-      if (message.member.hasPermission("MANAGE_SERVER")) {
+      if (message.member.hasPermission("ADMINSTRATOR")) {
         var r = Math.floor(Math.random() * (codes.length));	
         let pic = codes[r];	
         let meme = memes[r];
