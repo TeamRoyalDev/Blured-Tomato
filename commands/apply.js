97c1e72671d41cadf37e089b2e2fa522a04fa194
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-04 05:31:03 -0700

    Create apply.js

---
+++ commands/apply.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    if (message.member.hasPermission("ADMINISTRATOR")) return message.reply("You already have staff.");
+    if (!message.member.hasPermission("ADMINISTRATOR")) {
+        let channel = message.channel;
+        
+        let bicon = bot.user.displayavatarURL;
+        let applyEmbed = new Discord.RichEmbed()
+        .setAuthor("༺▬▬ ❰ Apply ❱ ▬▬༻")
+        .setThumbnail(bicon)
+        .setColor("#e44444")
+        .addField("Interested to join our team?", "https://goo.gl/forms/W7Z5ruOgjtXB4C3u1")
+        .setFooter("༺▬▬ ❰ Apply ❱ ▬▬༻");
+      
+        return message.channel.send(applyEmbed);
+            
+            message.delete();
+            channel.send(applyEmbed);
+    }
+}
+
+module.exports.help = {
+    name: "apply"
+}
