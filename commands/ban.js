e8396784b210779bc8ce58fee49c8d60d2c9214e
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-05 03:09:49 -0700

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,24 +4,10 @@ module.exports.run = async (bot, message, args) => {
   let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!bUser) return message.channel.send("Can't find user!");
   let bReason = args.join(" ").slice(22);
-  if(!message.member.hasPermission("SEND_MESSAGES")) return message.channel.send("No permissions!");
-  if(bUser.hasPermission("ADMINISTRATOR")) return message.channel.send("That person can't be kicked!");
-
-  let banEmbed = new Discord.RichEmbed()
-  .setDescription("⋅ Ban")
-  .setColor("#a80101")
-  .addField("Banned User", `${bUser}`)
-  .addField("Banned By", `<@${message.author.id}>`)
-  .addField("Banned In", message.channel)
-  .addField("Time", message.createdAt)
-  .addField("Reason", bReason)
-  .setFooter("Made by Chelsea ✦");
-
-  let punishmentschannel = message.guild.channels.find(`name`, "staff-chat");
-  if(!punishmentschannel) return message.channel.send("Can't find punishments channel.");
-
-  message.guild.member(bUser).ban(bReason);
-  punishmentschannel.send(banEmbed);
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No permissions!");
+  if(bUser.hasPermission("MANAGE_MEMBERS")) return message.channel.send("That person can't be kicked!");
+  
+  message.channel.send("ez bye");
 }
 
 module.exports.help = {
