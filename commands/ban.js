2942758c11bf31a204d20fea02e2df36cc5b522f
TeamRoyalDev <38868964+TeamRoyalDev@users.noreply.github.com>
2018-05-05 03:12:21 -0700

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,8 +4,8 @@ module.exports.run = async (bot, message, args) => {
   let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!bUser) return message.channel.send("Can't find user!");
   let bReason = args.join(" ").slice(22);
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No permissions!");
-  if(bUser.hasPermission("MANAGE_MEMBERS")) return message.channel.send("That person can't be kicked!");
+  if(!message.member.hasPermission("SEND_MESSAGES")) return message.channel.send("No permissions!");
+  if(bUser.hasPermission("ADMINISTRATOR")) return message.channel.send("That person can't be kicked!");
   
   message.channel.send("ez bye");
 }
